b08ca06c382372993056560f7867b33ffe671dd0
compsci <compsci@compsci>
2017-10-20 10:46:28 -0600

    Working except for the z wrap

---
+++ src/array_cipher/BeginHere.java
@@ -0,0 +1,23 @@
+package array_cipher;
+
+import java.util.Scanner;
+
+public class BeginHere {
+
+	public static void main(String[] args) {
+		Scanner kbd = new Scanner(System.in);
+		
+		CaesarCipher myCipher = new CaesarCipher();
+		
+		System.out.println("Please enter the plain text to encode.");
+		String plainText = kbd.nextLine();
+		
+		System.out.println("Please enter the key:");
+		int key = kbd.nextInt();
+		System.out.println("Your key is: " + key);
+		
+		
+		String codedText = myCipher.encode(plainText, key);
+		System.out.println("Your secret message is: " + codedText);
+	}
+}
